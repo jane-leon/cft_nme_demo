1fd7419b3de1d6b7ced20dbadc89706102461e15
Gregory Parent <gregparent2006@gmail.com>
2025-03-01 18:29:32 -0500

    Worked on js and learned some basics

---
+++ basics.js
@@ -0,0 +1,37 @@
+// this is how we would print stuff
+// console.log("Hello")
+
+// so the way we create variables
+let name = "Greg"
+
+var name2= "Gregory"
+
+// console.log(name)
+
+const name3="Michael"
+
+// console.log(name3)
+
+// name3="Steve";
+
+//we are going to create an array
+let nums = [1,2,3,4,5]
+
+//we set a boolean variable 
+let found = false
+
+//now iterate over the array
+for(let i = 0; i<nums.length;i++)
+{
+    if(nums[i]==3)
+    {
+        found = true;
+        console.log("We have found 3 at index " + i);
+        break;
+    }
+}
+
+if(!found)
+{
+    console.log("We didn't have a 3");
+}
