1b60114b875c4c640bb4b658089ca526b3d3dbd3
Gregory Parent <gregparent2006@gmail.com>
2025-03-13 12:44:57 -0400

    added gregs pfp

--- profiles/src/Profile.jsx
+++ profiles/src/Profile.jsx
@@ -1,20 +1,20 @@
 import profilePic1 from "./assets/jane.png"
-import profilePic2 from "./assets/jane.png"
+import profilePic2 from "./assets/gregory_parent_headshot copy.jpeg"
 
 function Profile(){
     return(
 
     <div className="profile-container">
         <div className="profile">
-        <img className="profileImg" src={profilePic1} alt="Jane's profile picture" />
-        <h2 className="profile-title">Jane Tenecota Villa</h2>
-        <p className="profileText">Love data, running, and my dog</p>
+            <img className="profileImg" src={profilePic1} alt="Jane's profile picture" />
+            <h2 className="profile-title">Jane Tenecota Villa</h2>
+            <p className="profileText">Love data, running, and my dog</p>
         </div>
 
         <div className="profile">
-        <img className="profileImg" src={profilePic2} alt="Gregory's profile picture" />
-        <h2 className="profile-title">Gregory Parent</h2>
-        <p className="profileText">Another type of shit</p>
+            <img className="profileImg" src={profilePic2} alt="Gregory's profile picture" />
+            <h2 className="profile-title">Gregory Parent</h2>
+            <p className="profileText">Locked in</p>
         </div>
   </div>
 
